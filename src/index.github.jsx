148cccd1bf359bf0a22a8bb39e18eb0e8a03c1de
MrFanggesun <mickshifangge@gmail.com>
2025-12-13 13:11:37 +0800

    迭代

--- src/index.github.jsx
+++ src/index.github.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.github';
-import './styles/index.css';
+import './styles/global.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
